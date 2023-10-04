605ab1d2070c60d1c69a855e95e712eca5dc8e6a
Yukyung Kim / Sally <rladbrud12@naver.com>
2023-10-04 22:46:40 +0900

    feat : openMultiWindow()

--- index.js
+++ index.js
@@ -1,5 +1,33 @@
-function openMultiWindow(string) {
-  return string;
+function openMultiWindow(windows, options, callback) {
+  const openedWindows = [];
+
+  function openNextWindow(index) {
+    if (index < windows.length) {
+      const { url, name } = windows[index];
+      const win = window.open(url, name || "", ...options);
+
+      if (win) {
+        openedWindows.push(win);
+        win.addEventListener("load", () => {
+          openNextWindow(index + 1);
+        });
+      } else {
+        console.error(`Failed to open window at index ${index}`);
+        openNextWindow(index + 1);
+      }
+    } else {
+      if (typeof callback === "function") {
+        callback(openedWindows);
+      }
+    }
+  }
+
+  openNextWindow(0);
 }
+
+// 예시 사용법:
+// openMultiWindow(windows, options, (openedWindows) => {
+//   // 모든 창이 열린 후에 실행할 코드
+// });
 
 module.exports = openMultiWindow;
